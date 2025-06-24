862f15ca25cb083e7bf42b86a30193256309fa46
chammyae <chammyaequo@gmail.com>
2025-06-25 00:08:34 +0700

    code correct
    missing geom mapping code fixed.

--- day_003/penguins.R
+++ day_003/penguins.R
@@ -55,5 +55,45 @@ ggplot(
     data = penguins,
     mapping = aes(x = flipper_length_mm, y = body_mass_g)
 ) +
-  geom_point(aes(color = species)) +
-  geom_smooth(method = "lm"))
+  geom_point(mapping = aes(color = species)) +
+  geom_smooth(method = "lm")
+
+  # It's not the end yet, I want to change the shape for each species.
+  ggplot(
+    data = penguins,
+    mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(mapping = aes(color = species, shape = species)) +
+  geom_smooth(method = "lm")
+
+  # And, it is time to add some labels to the plot. So, I will use labs() function to add lables.
+  # And, as an extra layer, I will use color blindness-friendly palette from the ggthemes package.
+  # So, I will install ggthemes package first.
+install.packages("ggthemes")
+# Then, I will load the package
+library(ggthemes)
+# Finally, I will add the labels and the color palette to the plot
+ggplot(
+    data = penguins,
+    mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(mapping = aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body Mass and Flipper Length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper Length (mm)",
+    y = "Body Mass (g)",
+    color = "Species",
+    shape = "Species"
+  ) +
+  scale_color_colorblind() # Using color blindness-friendly palette from ggthemes
+# Now, I have a complete plot with labels and color palette.
+# I can save the plot to a file using ggsave() function.
+ggsave("penguins_plot.png", width = 10, height = 6)
+
+# As I said before, I am new to R and I don't know where ggsave() function saves the file.
+# So, I will use getwd() function to get the current working directory.
+getwd()
+
+# Voila! It is under Documents folder.
