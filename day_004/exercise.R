72cc87bb7e120eac7f852d3b22b8a0221755f032
chammyae <chammyaequo@gmail.com>
2025-06-25 23:43:52 +0700

    code fixed
    fixed some error codes.

--- day_004/exercise.R
+++ day_004/exercise.R
@@ -16,14 +16,58 @@ library(palmerpenguins)
 library(ggplot2)
 ggplot(
     data = penguins,
-    mapping = aes(x = bill_length_mm, y = bill_depth_mm.)
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)
 )
 
 # As I have some experience with ggplot yesterday, I tell ggplot to plot the penguins data with which axis to use.
 ggplot(
     data = penguins,
-    mapping = aes(x = bill_length_mm, y = bill_depth_mm.)
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)
 ) +
     geom_point(
-        mapping = aes(color = species, shape = species)
+        mapping = aes(color = species, shape = species) +
 )
+
+# Now, I have a scatterplot of bill_depth_mm vs. bill_length_mm with points colored by species and a linear model line fitted to the data.
+ggplot(
+    data = penguins,
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)) +
+    geom_point(mapping = aes(color = species, shape = species)) +
+    geom_smooth(method = "lm")
+
+# Now, what we have is a scatterplot with smooth lines for each species. And let's add some labelling.
+ggplot(
+    data = penguins,
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)) +
+    geom_point(mapping = aes(color = species, shape = species)) +
+    geom_smooth(method = "lm") +
+    labs(
+        title = "Bill Depth vs. Bill Length",
+        subtitle = "Dimensions for Penguins' Culmen",
+        x = "Bill Length (mm)",
+        y = "Bill Depth (mm)",
+        color = "Species",
+        shape = "Species")
+
+# Now, I have a complete scatterplot with labels and color palette.But I want to add one more thing.
+# All data visualization must be accessible, so I will use the ggthemes package to add a color blindness-friendly palette.
+# Before that, I need to load ggthemes package.
+library(ggthemes)
+ggplot(
+    data = penguins,
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)) +
+    geom_point(mapping = aes(color = species, shape = species)) +
+    geom_smooth(method = "lm") +
+    labs(
+        title = "Bill Depth vs. Bill Length",
+        subtitle = "Dimensions for Penguins' Culmen",
+        x = "Bill Length (mm)",
+        y = "Bill Depth (mm)",
+        color = "Species",
+        shape = "Species") +
+    scale_color_colorblind() # Using color blindness-friendly palette from ggthemes
+
+    # Let's save the plot to a file using ggsave() function.
+ggsave("bill_depth_vs_bill_length.png", width = 10, height = 6)
+
+# Voila! Exercise completed.
